9d34bae18c1cb980c9eacb0e1d07cebec89f3a49
Sahil Rajput <rajputsahil.2204@gmail.com>
2020-02-28 22:03:48 +0530

    Club Booking Process Done

---
+++ Club23/Club/forms.py
@@ -0,0 +1,33 @@
+from  django import forms
+from .models import Bookclub
+
+class DateInput(forms.DateInput):
+    input_type = 'Date'
+    format = '%m/%d/%Y'
+    input_formats = '%m/%d/%Y'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'Time'
+
+class Bookclub_from(forms.ModelForm):
+    class Meta:
+        model = Bookclub
+        fields = '__all__'
+
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter Email', 'class': 'form-control'}),
+            'Occasion': forms.Select(attrs={'placeholder': 'Select Occasion', 'class': 'form-control'}),
+            'Time_from': TimeInput(attrs={'placeholder': 'Select Time From', 'class': 'form-control'}),
+            'Time_to': TimeInput(attrs={'placeholder': 'Select Time To', 'class': 'form-control'}),
+            'Date_from': DateInput(attrs={'placeholder': 'Select Date From', 'class': 'form-control'}),
+            'Date_to': DateInput( attrs={'placeholder': 'Select Date From', 'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(Bookclub_from, self).__init__(*args, **kwargs)
+        self.fields['email'].label = 'Email'
+        self.fields['Occasion'].label = 'Occasion'
+        self.fields['Time_from'].label = 'Time From'
+        self.fields['Time_to'].label = 'Time To'
+        self.fields['Date_from'].label = 'Date From'
+        self.fields['Date_to'].label = 'Date To'
